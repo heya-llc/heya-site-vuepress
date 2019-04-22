712b8cc9fa69ab004511ad5c06aab2be93cf217c
Sam Sherwood <sam@sherwoods.me>
2019-04-22 11:01:37 -0400

    Vue-GTM SSR Edit
    Attempting to get VuePress to use Vue-GTM

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -8,11 +8,13 @@ export default ({
     }) => {
     // ...apply enhancements to the app
 
-    Vue.use(VueGtm, {
-        id: 'GTM-5NXFLKJ', // Your GTM ID
-        enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
-        debug: true, // Whether or not display console logs debugs (optional)
-        vueRouter: router, // Pass the router instance to automatically sync with router (optional)
-        // ignoredViews: ['homepage'] // If router, you can exclude some routes name (case insensitive) (optional)
-    });
+    if (typeof process !== 'undefined' && process.isClient) {
+        Vue.use(VueGtm, {
+            id: 'GTM-5NXFLKJ', // Your GTM ID
+            enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+            debug: false, // Whether or not display console logs debugs (optional)
+            vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+            // ignoredViews: ['homepage'] // If router, you can exclude some routes name (case insensitive) (optional)
+        });
+    }
 }
