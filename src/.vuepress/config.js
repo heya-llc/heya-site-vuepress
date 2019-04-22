b775df0e3216750a9a98ebb18a321394beede840
Sam Sherwood <sam@sherwoods.me>
2019-04-22 10:30:49 -0400

    GTM Installation
    GTM Installed
    OpenGraph Image created

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,6 +8,10 @@ module.exports = {
         ['link', { rel: 'manifest', href: '/site.webmanifest' }],
         ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color:'#3c001d' }],
         ['meta', { name: 'theme-color', content: '#ffffff' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }]
+        ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+        ['meta', { property: 'og:image', content:'/opengraph-default.png' }],
+        ['meta', { property: 'og:image:width', content:'1200' }],
+        ['meta', { property: 'og:image:height', content:'628' }],
+        ['meta', { property: 'og:description', content:'A digital practice bridging the gap between insights and execution for marketing, engineering and design teams at scale.' }],
     ],
 }
