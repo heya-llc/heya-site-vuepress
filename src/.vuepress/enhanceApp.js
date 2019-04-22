a7fe91c1a2ab5a0c0e1befc16e894061242c7e23
Sam Sherwood <sam@sherwoods.me>
2019-04-22 12:39:11 -0400

    GTM test for window existing
    Will likely not function in live environment

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -1,1 +1,19 @@
-// Removed Vue-GTM
+import VueGtm from 'vue-gtm';
+
+export default ({
+    Vue, // the version of Vue being used in the VuePress app
+    options, // the options for the root Vue instance
+    router, // the router instance for the app
+    siteData // site metadata
+    }) => {
+    // ...apply enhancements to the app
+    if (typeof window !== 'undefined') {
+        Vue.use(VueGtm, {
+            id: 'GTM-5NXFLKJ', // Your GTM ID
+            enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+            debug: false, // Whether or not display console logs debugs (optional)
+            // vueRouter: router, // Pass the router instance to automatically sync with router (optional)
+            // ignoredViews: ['homepage'] // If router, you can exclude some routes name (case insensitive) (optional)
+        });
+    }
+}
